57cea65050a406b1e2650bcdea1e7586797bb704
Martin Vilcans <martin@librador.com>
2019-10-01 22:28:48 +0200

    Add ggez and render some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,74 @@
-fn main() {
-    println!("Hello, world!");
+#![windows_subsystem = "windows"]
+
+//use cgmath;
+use ggez::nalgebra::Point2;
+
+use ggez;
+use ggez::event;
+use ggez::graphics;
+use ggez::nalgebra as na;
+use ggez::{Context, GameResult};
+
+use std::path;
+use std::env;
+
+struct MainState {
+    image1: graphics::Image,
+    pos_x: f32,
+}
+
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let image1 = graphics::Image::new(ctx, "/ld-logo.png")?;
+
+        let s = MainState {
+            image1: image1,
+            pos_x: 0.0
+        };
+        Ok(s)
+    }
+}
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        self.pos_x = self.pos_x % 800.0 + 1.0;
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+
+        let circle = graphics::Mesh::new_circle(
+            ctx,
+            graphics::DrawMode::fill(),
+            na::Point2::new(0.0, 0.0),
+            100.0,
+            2.0,
+            graphics::WHITE,
+        )?;
+        graphics::draw(ctx, &circle, (na::Point2::new(self.pos_x, 380.0),))?;
+
+        let dst = Point2::new(20.0, 20.0);
+        graphics::draw(ctx, &self.image1, (dst,))?;
+
+        graphics::present(ctx)?;
+        Ok(())
+    }
+}
+
+pub fn main() -> GameResult {
+    let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        path
+    } else {
+        path::PathBuf::from("./resources")
+    };
+
+    let builder = ggez::ContextBuilder::new("Ludum Dare 45", "Martin Vilcans")
+        .add_resource_path(resource_dir);
+
+    let (ctx, event_loop) = &mut builder.build()?;
+    let state = &mut MainState::new(ctx)?;
+    event::run(ctx, event_loop, state)
 }
